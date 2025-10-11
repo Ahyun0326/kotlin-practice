eecf5289b57a3fb3e48fb6fdc731318ba04a8508
Yeseo Kim <chinkl850@gmail.com>
2025-10-11 22:53:51 +0900

    feat: 접근제한자

---
+++ src/main/kotlin/chapter3/AccessTestClass.kt
@@ -0,0 +1,28 @@
+package chapter3
+
+class AccessTestClass {
+    var b = 2
+    public var a:Int = 1
+    private var c = 3
+
+    fun publicTest() {
+        println("public 입니다")
+    }
+
+    public fun publicTest2() {
+        println("public 입니다")
+    }
+
+    private fun privateTest() {
+        println("private 입니다")
+    }
+}
+
+fun main() {
+    var accessTestClass = AccessTestClass()
+
+    // 접근 불가
+//    accessTestClass.c
+//    accessTestClass.privateTest()
+
+}
