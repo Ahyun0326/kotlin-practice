48c5b37f7b254cb4a37108e03a1b518090593592
Yeseo Kim <chinkl850@gmail.com>
2025-10-11 23:01:17 +0900

    feat: 오버라이딩

--- src/main/kotlin/chapter3/InheritanceExample.kt
+++ src/main/kotlin/chapter3/InheritanceExample.kt
@@ -2,9 +2,9 @@ package chapter3
 
 fun main() {
     var bird = Bird("새")
-    var chicken = Chicken("닭")
-    var sparrow = Sparrow("참새")
-    var pigeon = Pigeon("비둘기")
+    var chicken = Chicken("닭",2 )
+    var sparrow = Sparrow("참새", "갈색")
+    var pigeon = Pigeon("비둘기", "서울")
 
     bird.fly()
     chicken.fly()
@@ -15,19 +15,37 @@ fun main() {
 open class Bird(
     var name: String
 ) {
-    fun fly() {
+    open fun fly() {
         println("${name} 날아요~")
     }
 }
 
-class Chicken(name: String) : Bird(name) {
+class Chicken(
+    name: String,
+    val age: Int,
+) : Bird(name) {
 
+    override fun fly() {
+        println("${age}살의 ${name}가 날아봅니다~ 꼬끼오!")
+    }
 }
 
-class Sparrow(name: String) : Bird(name) {
+class Sparrow(
+    name: String,
+    val color: String
+) : Bird(name) {
 
+    override fun fly() {
+        println("${color}의 ${name}이 날아봅니다~ 짹짹!")
+    }
 }
 
-class Pigeon(name: String) : Bird(name) {
+class Pigeon(
+    name: String,
+    val address: String
+) : Bird(name) {
 
+    override fun fly() {
+        println("${address} 살고있는 ${name}가 날아봅니다~ 구구!")
+    }
 }
